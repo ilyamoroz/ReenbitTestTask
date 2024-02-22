502c86b20e29927f2a164351312fda7b298dbeb0
Ilya Moroz <imoroz.my@gmail.com>
2024-02-22 13:54:10 +0200

    Add email validator

---
+++ ReenbitTestTask/webapi/EmailValidator.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace webapi
+{
+    public static class EmailValidator
+    {
+        public static bool IsValidEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+
+            try
+            {
+                string pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+                return Regex.IsMatch(email, pattern);
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+        }
+    }
+}
