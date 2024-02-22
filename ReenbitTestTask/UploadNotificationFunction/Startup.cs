340ad19f8a9b785f1b026d0d2b0d8cceea8d44c3
Ilya Moroz <imoroz.my@gmail.com>
2024-02-22 13:56:04 +0200

    Create Upload notification azure function

---
+++ ReenbitTestTask/UploadNotificationFunction/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using UploadNotificationFunction.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(UploadNotificationFunction.Startup))]
+namespace UploadNotificationFunction
+{
+	public class Startup : FunctionsStartup
+	{
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddScoped<IEmailService,EmailService>();
+            builder.Services.AddScoped<ISASService,SASService>();
+        }
+    }
+}
+
