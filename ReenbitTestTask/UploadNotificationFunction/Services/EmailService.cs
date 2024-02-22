f495de0a9df3b2f15bd66b08acfee24ba6dda604
Ilya Moroz <imoroz.my@gmail.com>
2024-02-22 13:56:38 +0200

    Add email service

---
+++ ReenbitTestTask/UploadNotificationFunction/Services/EmailService.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace UploadNotificationFunction.Services
+{
+	public class EmailService: IEmailService
+	{        
+        public void SendLetter(MailMessage message, NetworkCredential credential)
+        {
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+            smtpClient.EnableSsl = true;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = credential;
+            smtpClient.Send(message);
+        }
+    }
+}
+
---
+++ ReenbitTestTask/UploadNotificationFunction/Services/IEmailService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+namespace UploadNotificationFunction.Services
+{
+	public interface IEmailService
+	{
+		void SendLetter(MailMessage message, NetworkCredential credential);
+	}
+}
+
