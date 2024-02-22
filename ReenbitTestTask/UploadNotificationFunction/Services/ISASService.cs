958f34af7fd319dca36a1234ae9720261ccbfdf0
Ilya Moroz <imoroz.my@gmail.com>
2024-02-22 13:56:22 +0200

    Add SAS service

---
+++ ReenbitTestTask/UploadNotificationFunction/Services/ISASService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace UploadNotificationFunction.Services
+{
+    public interface ISASService
+    {
+        string GetBlobSASUrl(CloudBlob blob);
+    }
+}
---
+++ ReenbitTestTask/UploadNotificationFunction/Services/SASService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+
+namespace UploadNotificationFunction.Services
+{
+    public class SASService : ISASService
+    {
+        public string GetBlobSASUrl(CloudBlob blob)
+        {
+            var expiryTime = DateTimeOffset.UtcNow.AddHours(1);
+
+            var sasPolicy = new SharedAccessBlobPolicy()
+            {
+                Permissions = SharedAccessBlobPermissions.Read,
+                SharedAccessExpiryTime = expiryTime
+            };
+
+            string sasToken = blob.GetSharedAccessSignature(sasPolicy);
+
+            string sasUri = blob.Uri + sasToken;
+
+            return sasUri;
+        }
+    }
+}
